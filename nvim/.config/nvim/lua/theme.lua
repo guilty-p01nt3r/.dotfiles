7b62f70486a2aa5c453c84a47570898cd15cbd59
Vincenzo Simeone <vs994.programming@gmail.com>
2021-09-20 15:48:08 +0200

    fixed folder orders

--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -1,6 +1,10 @@
 vim.o.background = "dark" -- or "light" for light mode
 vim.g.gruvbox_contrast_dark = 'soft'
+vim.g.gruvbox_transparent_bg=1
 vim.cmd([[colorscheme gruvbox]])
+
+-- Workaround for creating transparent bg
+vim.cmd([[autocmd SourcePost * highlight Normal     ctermbg=NONE guibg=NONE |    highlight LineNr     ctermbg=NONE guibg=NONE |    highlight SignColumn ctermbg=NONE guibg=NONE]])
 
 require('lualine').setup(
 {
