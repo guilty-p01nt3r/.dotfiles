a7af31ad1d976ffbfe3520f4e2e4680ccb6c2925
Vincenzo Simeone <vs994.programming@gmail.com>
2023-02-09 14:29:52 +0100

    remove zsh

--- zsh/.config/zsh/main.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# Source all files in all directory in this folder
-for file in $ZSH_CUSTOM/*/*.zsh; do 
-	source $file
-done
-
--- zsh/.config/zsh/startup/keybindings.zsh
+++
@@ -1,7 +0,0 @@
-function user_key_bindings {
-    # Insert here your bindings
-    bindkey     '^F'    _tmux_sessionizer 
-    bindkey     '^y^y'  _copypath
-}
-
-user_key_bindings
