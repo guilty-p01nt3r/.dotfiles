0d758082e7e8f26749d52e4b7affbf6a59870bd2
Vincenzo Simeone <vs994.programming@gmail.com>
2024-01-17 10:50:05 +0100

    chore: updated setTemp script

--- i3/.config/i3/setTemp.sh
+++ i3/.config/i3/setTemp.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
+#!/usr/bin/bash
 
+cd /sys/class/drm/card1/device/gpu_od/fan_ctrl
 
+echo "Setting fan target"
+echo "85" > fan_target_temperature
+echo "c" > fan_target_temperature
+echo "Committed fan target"
 
-echo "0 45 23" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_curve
-echo "1 60 40" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_curve
-echo "2 70 50" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_curve
-echo "3 80 90" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_curve
-echo "4 90 100" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_curve
-echo "c" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_curve
+sleep 1
 
+echo "Setting fan curve into"
+echo "0 45 23"  >  fan_curve
+echo "1 60 40"  >  fan_curve
+echo "2 70 50"  >  fan_curve
+echo "3 80 90"  >  fan_curve
+echo "4 90 100" > fan_curve
+echo c > fan_curve
+echo "Committed fan curve"
 
-echo "85" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_target_temperature
-echo "c" > /sys/class/drm/card1/device/gpu_od/fan_ctrl/fan_target_temperature
