7bfdec113a51ff74a5fb002dae6dfbec90ecfd7e
Vincenzo Simeone <vs994.programming@gmail.com>
2024-03-07 19:04:07 +0100

    chore: added setTemp script for Linux 6.7 new GPU managent interface

--- i3/.config/i3/setTemp.sh
+++ i3/.config/i3/setTemp.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/bash
 
+#echo "Setting fan target"
+#echo "85" > fan_target_temperature
+#echo "c" > fan_target_temperature
+#echo "Committed fan target"
+
 cd /sys/class/drm/card1/device/gpu_od/fan_ctrl
-
-echo "Setting fan target"
-echo "85" > fan_target_temperature
-echo "c" > fan_target_temperature
-echo "Committed fan target"
-
-sleep 1
 
 echo "Setting fan curve into"
 echo "0 45 23"  >  fan_curve
 echo "1 60 40"  >  fan_curve
 echo "2 70 50"  >  fan_curve
-echo "3 80 90"  >  fan_curve
-echo "4 90 100" > fan_curve
+echo "3 80 70"  >  fan_curve
+echo "4 95 100" > fan_curve
 echo c > fan_curve
 echo "Committed fan curve"
 
